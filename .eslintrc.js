36b35e727039851f933844133bc50b49f8e89c11
Brian <bperin42@gmail.com>
2025-12-12 15:40:33 -0800

    move from cloud build to direct docker depoy

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    // Minimal rules to avoid conflicts
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.next/',
+    'packages/',
+  ],
+};
