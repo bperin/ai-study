2de1a9d60e54120b7e978e8ccf8291b2f4f99d71
Brian <bperin42@gmail.com>
2025-12-11 16:32:39 -0800

    first commit, scaffold backend api, add auth,

---
+++ packages/api/src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: LoginDto) {
+    return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @Post('register')
+  register(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
+  }
+}
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001);
+
+  const config = new DocumentBuilder()
+    .setTitle('Memorang AI Study API')
+    .setDescription('The Memorang AI Study API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  
+  fs.writeFileSync('./openapi.json', JSON.stringify(document));
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
